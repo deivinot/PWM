bf68ac91dab711c36a5a93f982272e6701993ce5
deivinot <davidmaso2001@gmail.com>
2024-05-01 20:27:25 +0100

    angular y firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { MainComponent } from './main/main.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { ListaComponent } from './lista/lista.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ListaComponent,
+    FooterComponent,
+    NavComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "firstproyect-74fdb",
+      "appId": "1:761511782041:web:42927daaa58622bb0fba08",
+      "storageBucket": "firstproyect-74fdb.appspot.com",
+      "apiKey": "AIzaSyCsxTV1XpylqE-pgAUDJ09iGu5NzKoO1oI",
+      "authDomain": "firstproyect-74fdb.firebaseapp.com",
+      "messagingSenderId": "761511782041"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
